e505e8f29dc23561c6d3a525cbf129c380c02976
Jordan Adkins <36913671+JordanAdkins@users.noreply.github.com>
2020-03-02 15:17:05 -0500

    Initialized Backend

---
+++ src/main/java/com/gracefulmuse/creativeray/CreativerayApplication.java
@@ -0,0 +1,13 @@
+package com.gracefulmuse.creativeray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativerayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativerayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gracefulmuse/creativeray/CreativerayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracefulmuse.creativeray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativerayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
